ac16510f7829aadc3c2a6b365b97b35458c5af86
maksymbashkatov <maksymbashkatov@gmail.com>
2025-01-12 21:25:34 +0300

    create base next.js project with start configuration

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata, Viewport } from 'next';
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'WebSensei APP',
+  description: 'Test description',
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='ru'>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
