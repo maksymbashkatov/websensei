5a564b089c17d1ba9b0cf7b4d0fc0755ad5dd124
maksymbashkatov <maksymbashkatov@gmail.com>
2025-02-07 05:15:36 +0300

    refactor(app/layout.tsx): add yandex verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import Footer from './components/footer';
 export const metadata: Metadata = {
   title: 'Репетитор по программированию онлайн',
   description: 'Репетитор по программированию онлайн. Доступны как индивидуальные занятия по веб-программированию, так и менторинг. Создаём сайты с нуля для любых целей.',
+  other: {
+    'yandex-verification': 'b84c98b2b82b3ada',
+  }
 };
 
 export const viewport: Viewport = {
